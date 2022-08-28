ebb9125739d340e49bf80c344f3ab5e53ef664e3
diego <diegoalves32@gmail.com>
2022-08-28 13:00:03 -0300

    atual3

--- lampada.js
+++ lampada.js
@@ -1,8 +1,13 @@
 var clicks = 0
 
 function ligaDesliga(filename){
-    document.querySelector('.imagem').setAttribute('src','imagens/'+filename)
-    clicks++
-    document.querySelector('.clicks').innerHTML = clicks
+    if (clicks <= 5){
+        document.querySelector('.imagem').setAttribute('src','imagens/'+filename)
+        clicks++
+        document.querySelector('.clicks').innerHTML = clicks
+    } else {
+        document.querySelector('.text').innerHTML = 'Você queimou a lâmpada'
+        document.querySelector('.imagem').setAttribute('src','imagens/'+'desligada.png')
+    }
 }
 
