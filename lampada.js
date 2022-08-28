fffadddcf61461a2f54043653ea1638578351bb0
diego <diegoalves32@gmail.com>
2022-08-28 12:41:32 -0300

    contador

--- lampada.js
+++ lampada.js
@@ -1,5 +1,8 @@
+var clicks = 0
 
 function ligaDesliga(filename){
     document.querySelector('.imagem').setAttribute('src','imagens/'+filename)
+    clicks++
+    document.querySelector('.clicks').innerHTML = clicks
 }
 
